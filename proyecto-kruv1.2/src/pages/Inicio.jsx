0d1195c4dce6e282f0c03b2902d935e97e816401
alex558512345 <alexander_7797@hotmail.com>
2022-10-18 22:01:48 -0300

    SubidaVersion1.2
    Añadido pagina de "ingreso de empleados"

---
+++ proyecto-kruv1.2/src/pages/Inicio.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+
+
+const Inicio= () => {
+    return (
+        <div>
+            <div className='box-text'>
+                <h1>Bienvenido de vuelta.</h1>
+                <h2 className='parrafo'>Puedes realizar tus actividades que estan disponibles para ti!</h2>
+            </div>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-md-4'>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="https://es.justexw.com/wp-content/uploads/sites/2/importar-datos-web-en-excel.jpg" />
+                            <Card.Body>
+                                <Card.Title>Agrega tus empleados!</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the
+                                        bulk of the card's content.
+                                    </Card.Text>
+                            </Card.Body>
+                    </Card>   
+                    </div>
+                    <div className='col-md-4'>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="https://trucoslondres.com/wp-content/uploads/2017/04/people-1.jpg" />
+                            <Card.Body>
+                                <Card.Title>Revisa la informacion de tus empleados</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the
+                                        bulk of the card's content.
+                                    </Card.Text>
+                            </Card.Body>
+                    </Card>  
+                    </div>
+                    <div className='col-md-4'>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="https://www.workmeter.com/wp-content/uploads/2022/07/beneficios-antiguedad-empleado-scaled.jpg" />
+                            <Card.Body>
+                                <Card.Title>Pagale a tus empleados!</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the
+                                        bulk of the card's content.
+                                    </Card.Text>
+                            </Card.Body>
+                    </Card>  
+                    </div>
+                </div>
+            </div>
+        </div>     
+    );
+};
+
+export default Inicio;
---
+++ proyecto-kruv1.2/src/pages/Perfiles.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+
+
+
+
+const Perfiles = () => {
+    return (
+        <div>
+            <div className='box-text'>
+                <h1>Perfiles</h1>
+                <h2 className='parrafo'>Aqui puedes ver los empleados que fueron ingresados al sistema.</h2>
+            </div>
+            <div className='box3'>
+                <Table striped bordered hover  variant='dark' >
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Rut</th>
+                            <th>Telefono</th>
+                            <th>Direccion</th>
+                            <th>Acciones
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>
+                            <ButtonGroup aria-label="Basic example">
+                                <Button variant="secondary">Editar</Button>
+                                <Button variant="secondary">Eliminar</Button>
+                            </ButtonGroup>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                            <td>
+                            <ButtonGroup aria-label="Basic example">
+                                <Button variant="secondary">Editar</Button>
+                                <Button variant="secondary">Eliminar</Button>
+                            </ButtonGroup>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td colSpan={2}>Larry the Bird</td>
+                            <td>@twitter</td>
+                            <td>
+                            <ButtonGroup aria-label="Basic example">
+                                <Button variant="secondary">Editar</Button>
+                                <Button variant="secondary">Eliminar</Button>
+                            </ButtonGroup>
+                            </td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </div>
+                
+
+        </div>
+    );
+};
+
+export default Perfiles;
